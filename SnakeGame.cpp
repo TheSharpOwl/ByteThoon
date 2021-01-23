0b56af35fb9761edad8b9c2dd25d2e5aad152c72
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-01-23 16:32:00 +0300

    Added start function. Going to do the drawing functions to see what is going on in the next stages.

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <random>
+#include <ctime>
+#include <cstdlib>
 
+const uint32_t Width = 100, Height = 20;
+std::vector<std::pair<uint32_t, uint32_t>> bodyCoord;
+
+void draw(uint32_t Width, uint32_t Height)
+{
+    for (unsigned int i = 0; i <= Height + 1; i++)
+    {
+        for (unsigned int j = 0; j <= Width + 1; j++)
+        {
+            if (i == 0 || i == Height + 1 || j == 0 || j == Width + 1)
+                std::cout << "#";
+            else if (i == bodyCoord[0].second && j == bodyCoord[0].first)
+                std::cout << "*";
+            else
+                std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+void start()
+{
+    // using time as a seed for our random start point
+    std::srand((unsigned int) std::time(nullptr));
+    uint32_t startX = std::rand() % Width + 1;
+    uint32_t startY = std::rand() % Height + 1;
+
+    bodyCoord.push_back({startX, startY});
+}
 int main()
 {
-    std::cout << "Hello SnakeGame!\n";
+
 }
